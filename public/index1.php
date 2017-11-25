521fa7524b5f2e9a3bf6d21b377dc2a5407ad4b4
Lasanga Guruge <lasangaguruge@gmail.com>
2017-11-25 14:42:14 +0530

    replaced with all files after css v4.0.0-beta2

---
+++ public/index1.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- tab-icon of the website -->
+    <link rel="icon" type="image/x-icon" href="images/main_icon.png" >
+
+    <title>Save city</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css-2/css/bootstrap.min.css">
+
+    <!-- custom CSS -->
+    <link rel="stylesheet" href="css-2/custom/header.css">
+    <link rel="stylesheet" href="css-2/custom/footer.css">
+
+  </head>
+  <body>
+    <!-- Navigation -->
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <div class="container">
+          <h3 style="color: #ffffff;margin-bottom:0px">Public</h3>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="index.php">Home
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="about.php">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="services.php">Services</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="map.php">Map</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#footer">Contact</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="login.php">Login</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    
+    <!-- header image -->
+    <header class="py-4 bg-image-full" style="background-image: url('images/fire.jpeg');">
+      <div class="container">
+        <br />
+        <h1>Disasters on time</h1>
+        <br>
+        <div class="description">
+          <p>Community participation in rescue and relief operations and reconstruction after a disaster is always essential.</p>
+          <p>It is a good sign that everyone starts feeling the gravity of the situation and
+          </p>
+          <p> comes forward with a helping hand.</p>
+        </div>
+        <div style="margin-top: 20px;">
+          <button class="btn btn-danger" id="subscribe" onclick="subscribe(this) ">Subscribe</button>
+        </div>  
+      </div>
+      </header>
+
+      <br />
+
+
+      <footer class ="py-4 bg-dark" id="footer">
+        <div class="container">
+          <div class="menu-items">
+            <div class="table-responsive">
+              <table class="table">
+                <td>
+                <ul>
+                  <li>contact us:<dl><dt>011-2-566444</dt><dt>011-5-887-997</dt> <dt>0778-98-45-666</dt></dl></li>
+                </ul>
+                </td>
+                <td>
+                <ul>
+                  <li>email:<dl><dt>SampleDisasterManagment@gmail.com</dt></dl>
+                  </li>
+                </ul>
+                </td>
+                <td>
+                <ul>
+                  <li>Fax:<dl><dt>+00976654321</dt></dl>
+                  </li>
+                </ul>
+                </td>
+              </table>
+            </div>
+          </div>
+                <p class="mbr-text mb-0 mbr-fonts-style mbr-white display-7">
+                    © Copyright 2017 Disaster Managment Centre - All Rights Reserved
+                </p>
+            </div>
+      </footer>
+
+
+
+
+
+
+
+    <!-- jQuery first, then Tether, then Bootstrap JS. -->
+    <script src="js-2/jQuery/jquery-3.2.1.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+
+    <!-- bootstrap core js -->
+    <script src="js-2/js/bootstrap.min.js"></script>
+    <script src="js-2/js/bootstrap.bundle.min.js"></script>
+    <script src="js-2/custom/java.js"></script>
+
+  </body>
+</html>
